e7ac654d8d98b7271894ff777171befe7a47db85
_ Himanshu <himanshu@uark.edu>
2025-02-03 21:15:28 -0600

    basic crew ai agent

--- src/agentic_market_analysis/crew.py
+++ src/agentic_market_analysis/crew.py
@@ -44,7 +44,7 @@ class AgenticMarketAnalysis():
 	def reporting_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			output_file='output/report.md'
 		)
 
 	@crew
